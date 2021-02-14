65c87e39f47ee35d7d71aea762d3595baa81b740
sudhirchitla <sudhirchitla@gmail.com>
2021-02-14 14:25:50 +0530

    Add files via upload

--- cricket.js
+++ cricket.js
@@ -3,6 +3,8 @@ var cricMatches='https://origin-apinew.cricket.com.au/matches';
 
 
 var cricUri = 'https://origin-apinew.cricket.com.au/matches/2738/49930/current';
+var cricUri1 = 'https://origin-apinew.cricket.com.au/matches/';
+
 
 var battingwheelUri = 'https://origin-apinew.cricket.com.au/battingwheel/2738/49930/1';
 
@@ -10,61 +12,110 @@ var cricMatch = [];
 var cricInfo = '';
 
 
+
+
+
 async function showCricMatch() {
 
     let matchResponse = await fetch(cricMatches);
     let matchData = await matchResponse.json();
-    //console.log( matchData );
+    console.log( matchData );
+    let mytitle = '';
 
-    let displayMatches = '';
+    let showContents = document.getElementById('matches');   // Content holder 
+
 
     if(matchData.meta.inProgressMatchCount) {
         matchData.matchList.matches.forEach(element => {
-            displayMatches += '<h2>'+element.series.name + '<br>';
-            displayMatches += element.name + '<br>' + element.matchSummaryText.bold() + '</h2><br>';
-            document.title = element.matchSummaryText;
+
+            let displayMatches = '';
+
+            displayMatches += ''+element.homeTeam.shortName + ' vs ' + element.awayTeam.shortName + '<br>';
+            displayMatches += element.name + '<br>' + element.matchSummaryText.small() + '<br>';
+
+            let matchUri = cricUri1 +element.series.id +'/'+ element.id+'/current/';
+
+
+            let col = document.createElement("div");    
+            col.setAttribute("class", "col-12 text-center text-dark p-2 font-weight-bolder");   
+
+            let myContent = document.createElement("p");
+            myContent.innerHTML = displayMatches;
+
+            col.append(myContent);
+            showContents.append(col);
+
+
+            //document.getElementById('info').innerHTML = displayMatches;
+
+            showMatchDetails(matchUri);
+
+            mytitle += element.matchSummaryText;
+
         });
+
+        document.title = mytitle;
+
 
     }
 
     else    displayMatches = 'None of the matches are currently in progress';
-
-    document.getElementById('matches').innerHTML = displayMatches;
-
-
-
-    let cricResponse = await fetch(cricUri);
-    let cricData = await cricResponse.json();
-
-    //console.log( cricData );
-
-    let display = '';
-
-    display += cricData.meta.series.name.bold() + '<br>';
-    display += cricData.meta.venueName + '<br>';
-    display += cricData.matchDetail.teamBatting.name + ' ' 
-                + "<h4 class='bg-danger'>" + cricData.matchDetail.teamBatting.score + '</h4>';
-
-    let displayDetails = '';
-    let batter1 = cricData.matchDetail.currentBatters[0].name 
-                + ' ' + cricData.matchDetail.currentBatters[0].runs
-                + ' (' + cricData.matchDetail.currentBatters[0].ballsFaced + ')';
-    let batter2 = cricData.matchDetail.currentBatters[1].name 
-                + ' ' + cricData.matchDetail.currentBatters[1].runs
-                + ' (' + cricData.matchDetail.currentBatters[1].ballsFaced + ')';
-    let bowler = cricData.matchDetail.bowler.name 
-                + ' ' + cricData.matchDetail.bowler.bowlerOver 
-                + ' ' + cricData.matchDetail.bowler.runsAgainst
-                + ' ' + cricData.matchDetail.bowler.wickets
-                + ' ' + cricData.matchDetail.bowler.economy;
-
-
-    displayDetails += batter1 + '<br>'+ batter2 + '<br>'+ bowler;
-
-    document.getElementById('info').innerHTML = display;
-
-    document.getElementById('details').innerHTML = displayDetails;
+    
 
 }
+
+
+async function showMatchDetails(matchUri){
+
+    let matchResponse = await fetch(matchUri);
+    let matchData = await matchResponse.json();
+
+    console.log(matchUri);
+     
+    let display = '';
+    let displayDetails = '';
+    let matchInfo ='';
+
+
+    display += matchData.matchDetail.teamBatting.name + ' ' 
+                + "<h6 class='bg-danger'>" + matchData.matchDetail.teamBatting.score + '</h6>';
+                
+    let batter1 = matchData.matchDetail.currentBatters[0].name 
+                + ' ' + matchData.matchDetail.currentBatters[0].runs
+                + ' (' + matchData.matchDetail.currentBatters[0].ballsFaced + ')';
+    let batter2 = matchData.matchDetail.currentBatters[1].name 
+                + ' ' + matchData.matchDetail.currentBatters[1].runs
+                + ' (' + matchData.matchDetail.currentBatters[1].ballsFaced + ')';
+    let bowler = matchData.matchDetail.bowler.name 
+                + ' ' + matchData.matchDetail.bowler.bowlerOver 
+                + ' ' + matchData.matchDetail.bowler.runsAgainst
+                + ' ' + matchData.matchDetail.bowler.wickets
+                + ' ' + matchData.matchDetail.bowler.economy;
+    displayDetails += batter1 + '<br>'+ batter2 + '<br>'+ bowler.small()+ '<br>';
+
+    matchInfo += display + displayDetails;
+
+    let showContents = document.getElementById('info');   // Content holder 
+
+    let col = document.createElement("div");    
+    col.setAttribute("class", "col-12 text-center bg-info p-1");   
+
+    let myContent = document.createElement("p");
+    myContent.innerHTML = matchInfo;
+
+    col.append(myContent);
+    
+    showContents.append(col); 
+
+
+
+    //document.getElementById('details').innerHTML = display + displayDetails;
+
+    //document.getElementById('matches').appendChild(document.createTextNode(displayDetails)); 
+}
+
+
+
+
 
 showCricMatch();
